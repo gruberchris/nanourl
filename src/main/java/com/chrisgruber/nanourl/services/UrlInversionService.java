445e9a4f0322379d98e8345f48e47e27787e6b01
Chris Gruber <chris@chrisgruber.com>
2019-01-11 16:07:33 -0600

    encode/decode urls and nano urls

---
+++ src/main/java/com/chrisgruber/nanourl/services/UrlInversionService.java
@@ -0,0 +1,41 @@
+package com.chrisgruber.nanourl.services;
+
+public class UrlInversionService {
+    private String alphabet;
+    private int base;
+
+    public UrlInversionService() {
+        this.alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+        this.base = this.alphabet.length();
+    }
+
+    public UrlInversionService(String alphabet) {
+        this.alphabet = alphabet;
+        this.base = this.alphabet.length();
+    }
+
+    public String Encode(int i) {
+        if (i == 0) {
+            return Character.toString(this.alphabet.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i > 0) {
+            sb.append(this.alphabet.charAt(i % this.base));
+            i = i / this.base;
+        }
+
+        return String.join("", sb.reverse());
+    }
+
+    public int Decode(String s) {
+        int i = 0;
+
+        for (char c : s.toCharArray()) {
+            i = (i * this.base) + this.alphabet.indexOf(c);
+        }
+
+        return i;
+    }
+}
