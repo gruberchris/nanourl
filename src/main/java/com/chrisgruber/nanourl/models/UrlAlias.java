5a46825f4f5810781278195659cc4ecf91293949
Chris Gruber <chris@chrisgruber.com>
2019-01-11 17:25:08 -0600

    UrlAlias will need integer id for inversion

--- src/main/java/com/chrisgruber/nanourl/models/UrlAlias.java
+++ src/main/java/com/chrisgruber/nanourl/models/UrlAlias.java
@@ -1,25 +1,24 @@
 package com.chrisgruber.nanourl.models;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
 public class UrlAlias {
     @Id
-    public ObjectId id;
+    public int id;
 
     public String url;
     public String aliasUrl;
 
     public UrlAlias() {}
 
-    public UrlAlias(ObjectId id, String url, String aliasUrl) {
+    public UrlAlias(int id, String url, String aliasUrl) {
         this.id = id;
         this.url = url;
         this.aliasUrl = aliasUrl;
     }
 
-    public String get_id() { return this.id.toHexString(); }
-    public void set_id(ObjectId id) { this.id = id; }
+    public int get_id() { return this.id; }
+    public void set_id(int id) { this.id = id; }
 
     public String get_url() { return this.url; }
     public void set_url(String url) { this.url = url; }
--- src/main/java/com/chrisgruber/nanourl/repositories/UrlAliasRepository.java
+++ src/main/java/com/chrisgruber/nanourl/repositories/UrlAliasRepository.java
@@ -1,9 +1,8 @@
 package com.chrisgruber.nanourl.repositories;
 
 import com.chrisgruber.nanourl.models.UrlAlias;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UrlAliasRepository extends MongoRepository<UrlAlias, String> {
-    UrlAlias findById(ObjectId id);
+    UrlAlias findById(int id);
 }
