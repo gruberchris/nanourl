8f4f5579a9264bf0749a0daeeb1a8f6d20366761
Chris Gruber <chris@chrisgruber.com>
2019-01-12 14:40:26 -0600

    whitespace clean up

--- src/main/java/com/chrisgruber/nanourl/exceptions/NanoUrlEntityNotFoundException.java
+++ src/main/java/com/chrisgruber/nanourl/exceptions/NanoUrlEntityNotFoundException.java
@@ -3,6 +3,6 @@ package com.chrisgruber.nanourl.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No such nano url")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such nano url")
 public class NanoUrlEntityNotFoundException extends RuntimeException {
 }
--- src/main/java/com/chrisgruber/nanourl/exceptions/NanoUrlEntitySaveException.java
+++ src/main/java/com/chrisgruber/nanourl/exceptions/NanoUrlEntitySaveException.java
@@ -3,6 +3,6 @@ package com.chrisgruber.nanourl.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR, reason="Persistence operation failed")
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Persistence operation failed")
 public class NanoUrlEntitySaveException extends RuntimeException {
 }
