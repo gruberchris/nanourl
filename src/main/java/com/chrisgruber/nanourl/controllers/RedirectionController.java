9fadb59ff9a34ed7d57ce438aff7b00cbd468fbb
Chris Gruber <chris@chrisgruber.com>
2019-01-13 21:01:03 -0600

    no need to document the MVC controller in Swagger

--- src/main/java/com/chrisgruber/nanourl/controllers/RedirectionController.java
+++ src/main/java/com/chrisgruber/nanourl/controllers/RedirectionController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.View;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/u")
+@ApiIgnore
 public class RedirectionController {
     private UrlAliasRepository urlAliasRepository;
 
